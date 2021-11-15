361680cc456928b5f15c4864184433f3302d18a5
maczi01 <mateusz.w.twardy@gmail.com>
2021-11-15 22:38:15 +0000

    Initial commit

---
+++ src/main/java/com/feedback/app/FeedackAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.feedback.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedackAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeedackAppBackendApplication.class, args);
+    }
+
+}
