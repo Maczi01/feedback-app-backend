1926bbae9330fbab3f5e6f82696a82bbeb1ef2a2
MateuszTwardy <mateusz.w.twardy@gmail.com>
2021-11-17 23:09:27 +0000

    Fixed sql problems, added more data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,15 +3,15 @@ values (1, 'mati@mati.pl'),
        (2, 'mati@mati.pl'),
        (3, 'mati@mati.pl');
 
-insert into category (id, name, description)
-values (1, 'ELECTRONICS', 'agd, rtv and other electro stuff'),
-       (2, 'HOME', 'Things for kitchen, bathroom, garden'),
-       (3, 'SPORT', 'balls, sport wears, accessories');
-
 insert into product (id, name, grade)
 values (1, 'laptop', 5),
        (2, 'chair', 4),
        (3, 'ball', 3);
+
+insert into category (id, name, description, product_id)
+values (1, 'ELECTRONICS', 'agd, rtv and other electro stuff', 1),
+       (2, 'HOME', 'Things for kitchen, bathroom, garden',1),
+       (3, 'SPORT', 'balls, sport wears, accessories', 2);
 
 insert into feedback (id, title, description, date, user_id, product_id)
 values (1, 'best laptop', 'solid laptop, best ever', '2017-10-08 15:00:00', 1, 1),
