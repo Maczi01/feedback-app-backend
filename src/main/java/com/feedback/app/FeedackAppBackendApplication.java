c4106e8d72c7aa2feae57701ae25fda6758a3cbe
MateuszTwardy <mateusz.w.twardy@gmail.com>
2021-11-19 10:36:59 +0000

    Changed spring version in gradle file

--- src/main/java/com/feedback/app/FeedackAppBackendApplication.java
+++ src/main/java/com/feedback/app/FeedackAppBackendApplication.java
@@ -3,7 +3,7 @@ package com.feedback.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.feedback.app"})
 public class FeedackAppBackendApplication {
 
     public static void main(String[] args) {
