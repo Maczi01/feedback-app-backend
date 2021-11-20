f7170842049a0bbfca8c9bff92015fed8024a241
MateuszTwardy <mateusz.w.twardy@gmail.com>
2021-11-20 23:17:26 +0000

    Added package to scan (user package)

--- src/main/java/com/feedback/app/FeedbackAppBackendApplication.java
+++ src/main/java/com/feedback/app/FeedbackAppBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories
 @SpringBootApplication(scanBasePackages = {"com.feedback.app.feedback",
-        "com.feedback.app.product",})
+        "com.feedback.app.product","com.feedback.app.user"})
 public class FeedbackAppBackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/feedback/app/user/dto/UserDTO.java
+++ src/main/java/com/feedback/app/user/dto/UserDTO.java
@@ -1,15 +1,8 @@
 package com.feedback.app.user.dto;
 
-import com.feedback.app.feedback.model.Feedback;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
 
 @Builder
 @Getter
