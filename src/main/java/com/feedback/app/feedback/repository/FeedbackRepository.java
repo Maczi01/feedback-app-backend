90e995e18cf5098accae11ff40253355f1d95908
MateuszTwardy <mateusz.w.twardy@gmail.com>
2021-11-22 16:33:55 +0000

    Added parameter to query (FeedbackRepository)

--- src/main/java/com/feedback/app/feedback/repository/FeedbackRepository.java
+++ src/main/java/com/feedback/app/feedback/repository/FeedbackRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 
-    @Query("select f from Feedback as f inner join User u on f.user.id =  u.id")
+    @Query("select f from Feedback as f inner join User u on f.user.id =  u.id where f.user.id = :id")
     List<Feedback> getFeedbackByUserId(@Param("id") Long id);
 
 }
