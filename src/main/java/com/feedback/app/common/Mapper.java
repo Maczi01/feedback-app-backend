376841d854ff2593631f4b9bc5b3f0facf68d66e
MateuszTwardy <mateusz.w.twardy@gmail.com>
2021-11-29 16:16:26 +0000

    Removed unused imports from mapper

--- src/main/java/com/feedback/app/common/Mapper.java
+++ src/main/java/com/feedback/app/common/Mapper.java
@@ -1,10 +1,8 @@
 package com.feedback.app.common;
 
-import com.feedback.app.user.service.WrongEmailException;
-
 public interface Mapper<K, V> {
 
     V toEntity(K value);
 
-    K toDTO(V value) throws WrongEmailException;
+    K toDTO(V value);
 }
