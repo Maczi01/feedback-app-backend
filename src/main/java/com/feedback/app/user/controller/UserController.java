6f99d3e378cd0ffd9ca17b7c48d8a76821ef9e4f
MateuszTwardy <mateusz.w.twardy@gmail.com>
2021-11-30 17:11:58 +0000

    Changed user controller - remove too many responsibilities, add new exception for not found user.

--- src/main/java/com/feedback/app/user/controller/UserController.java
+++ src/main/java/com/feedback/app/user/controller/UserController.java
@@ -1,51 +1,26 @@
 package com.feedback.app.user.controller;
 
-import com.feedback.app.feedback.dto.FeedbackDTO;
-import com.feedback.app.feedback.mapper.FeedbackMapper;
-import com.feedback.app.feedback.service.FeedbackService;
 import com.feedback.app.user.dto.UserDTO;
-import com.feedback.app.user.mapper.UserMapper;
-import com.feedback.app.user.model.User;
 import com.feedback.app.user.service.UserService;
-import com.feedback.app.user.service.WrongEmailException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/v1/users")
 public class UserController {
 
     private UserService userService;
-    private UserMapper userMapper;
-    private FeedbackMapper feedbackMapper;
-    private FeedbackService feedbackService;
 
-    public UserController(UserService userService, UserMapper userMapper, FeedbackMapper feedbackMapper, FeedbackService feedbackService) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.userMapper = userMapper;
-        this.feedbackMapper = feedbackMapper;
-        this.feedbackService = feedbackService;
     }
 
     @GetMapping("/{id}")
     public UserDTO getUserById(@PathVariable Long id) {
-        User userById = userService.getUserById(id);
-        List<FeedbackDTO> feedbacksByUserId = feedbackService.getFeedbackByUserId(id)
-                .stream()
-                .map(feedback -> feedbackMapper.toDTO(feedback))
-                .collect(Collectors.toList());
-        UserDTO userDTO = userMapper.toDTO(userById);
-        userDTO.setFeedbackList(feedbacksByUserId);
-        return userDTO;
+        return userService.getUserById(id);
     }
 
     @PostMapping
     public UserDTO addNewUser(@RequestBody UserDTO userDTO) {
-        User user = userMapper.toEntity(userDTO);
-        User savedUser = userService.addNewUser(user);
-        return userMapper.toDTO(savedUser);
+        return userService.addNewUser(userDTO);
     }
-
 }
--- src/main/java/com/feedback/app/user/service/UserService.java
+++ src/main/java/com/feedback/app/user/service/UserService.java
@@ -1,23 +1,45 @@
 package com.feedback.app.user.service;
 
+import com.feedback.app.feedback.dto.FeedbackDTO;
+import com.feedback.app.feedback.mapper.FeedbackMapper;
+import com.feedback.app.product.model.Product;
+import com.feedback.app.product.service.ProductNotFoundException;
+import com.feedback.app.user.dto.UserDTO;
+import com.feedback.app.user.mapper.UserMapper;
 import com.feedback.app.user.model.User;
 import com.feedback.app.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
     private UserRepository userRepository;
+    private UserMapper userMapper;
+    private FeedbackMapper feedbackMapper;
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User getUserById(Long id) {
-        return userRepository.findById(id).orElseThrow(RuntimeException::new);
+    public UserDTO getUserById(Long id) {
+        User userById = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("Can not find product with id: " + id));
+        UserDTO userDTO = userMapper.toDTO(userById);
+        List<FeedbackDTO> collect = userById.getFeedbacks()
+                .stream()
+                .map(feedback -> feedbackMapper.toDTO(feedback))
+                .collect(Collectors.toList());
+        userDTO.setFeedbackList(collect);
+        return userDTO;
     }
 
-    public User addNewUser(User user) {
-        return userRepository.save(user);
+    public UserDTO addNewUser(UserDTO userDTO) {
+        User user = userMapper.toEntity(userDTO);
+        User savedUser = userRepository.save(user);
+        return userMapper.toDTO(savedUser);
     }
 }
+
