c887655432dac0a856d2df6b5142f868b674c49b
MateuszTwardy <mateusz.w.twardy@gmail.com>
2021-12-08 12:31:47 +0000

    Changed mapper (added feedback list and email)

--- src/main/java/com/feedback/app/user/mapper/UserMapper.java
+++ src/main/java/com/feedback/app/user/mapper/UserMapper.java
@@ -1,20 +1,26 @@
 package com.feedback.app.user.mapper;
 
 import com.feedback.app.common.Mapper;
+import com.feedback.app.feedback.dto.FeedbackDTO;
 import com.feedback.app.user.dto.UserDTO;
 import com.feedback.app.user.model.Email;
 import com.feedback.app.user.model.User;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class UserMapper implements Mapper<UserDTO, User> {
 
     @Override
     public UserDTO toDTO(User user) {
+        List<FeedbackDTO> feedbackList = new ArrayList<>();
         return UserDTO.builder()
                 .id(user.getId())
                 .email(user.getEmail().getEmail())
                 .name(user.getName())
+                .feedbackList(feedbackList)
                 .build();
     }
 
