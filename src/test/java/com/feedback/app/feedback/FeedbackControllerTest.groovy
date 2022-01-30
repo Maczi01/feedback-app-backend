db1b7ca002195aa11f5ac094f88fd70c271047f3
MateuszTwardy <mateusz.w.twardy@gmail.com>
2022-01-30 22:52:40 +0000

    Trying to fix tests

---
+++ src/test/java/com/feedback/app/feedback/FeedbackControllerTest.groovy
@@ -0,0 +1,12 @@
+package com.feedback.app.feedback
+
+import spock.lang.Specification
+
+class FeedbackControllerTest extends Specification {
+
+    def "when context is loaded then all expected beans are created"() {
+        expect:
+        1 == 1
+    }
+
+}
